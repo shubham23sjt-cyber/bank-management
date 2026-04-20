43ae2496e316ae833b8932b332dbe8c4b2ad0477
shubham23sjt-cyber <shubham23.sjt@gmail.com>
2026-04-20 13:21:25 +0530

    Update Auth Module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private auth: AuthService) {}
 
   @Post('register')
-  register(@Body() body:any){
-    return this.authService.register(body);    
+  register(@Body() dto: RegisterDto) {
+    return this.auth.register(dto);
   }
 
   @Post('login')
-  login(@Body() body:any){
-    return this.authService.login(body);
+  login(@Body() dto: LoginDto) {
+    return this.auth.login(dto);
   }
 }
